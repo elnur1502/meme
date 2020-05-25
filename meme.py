bc769f3c59ba3c461734983aef34a7c51915be9c
elnur1502 <62846915+elnur1502@users.noreply.github.com>
2020-05-25 16:17:19 +0500

    Update meme.py

--- meme.py
+++ meme.py
@@ -9,13 +9,8 @@ def get_html(site):
 
 def get_page_data(html):
     soup = BeautifulSoup(html, 'lxml')
-    div2 = soup.find('div', id='stream-2')
-    try:
-        img = div2.find_all('img').get
-        for src in img:
-            print img.get('src')
-    except:
-        pass
+    div2 = soup.find('div', class='main-wrap')
+    print(div2)
 
 
 def main():

7482bef8bf9f1ade03da69735ccbedcb08092fd6
elnur1502 <62846915+elnur1502@users.noreply.github.com>
2020-05-25 16:18:14 +0500

    Update meme.py

--- meme.py
+++ meme.py
@@ -9,7 +9,7 @@ def get_html(site):
 
 def get_page_data(html):
     soup = BeautifulSoup(html, 'lxml')
-    div2 = soup.find('div', class='main-wrap').get
+    div2 = soup.find('div', class='main-wrap').text
     print(div2)
 
 

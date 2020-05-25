8817358412ee049899042085691d7916129e63eb
elnur1502 <62846915+elnur1502@users.noreply.github.com>
2020-05-25 17:06:07 +0500

    Update meme.py

--- meme.py
+++ meme.py
@@ -10,7 +10,7 @@ def get_html(site):
 
 def get_page_data(html):
     soup = BeautifulSoup(html, 'lxml')
-    images = soup.findAll('img')
+    images = soup.find_all('img')
     print(images['src'])
 
 

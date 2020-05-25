e60b38e16876fba56cf53021b4be4e762bc8dcb9
elnur1502 <62846915+elnur1502@users.noreply.github.com>
2020-05-25 18:37:52 +0500

    Update meme.py

--- meme.py
+++ meme.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 from urllib2 import urlopen
-html_doc = urlopen('http://otus.ru').read()
+html_doc = urlopen('http://1001mem.ru').read()
 soup = BeautifulSoup(html_doc)
 
 for img in soup.find_all('img'):

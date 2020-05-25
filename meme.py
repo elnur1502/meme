463b6b28653026b3cdee077c0cfbe4f1079f8c41
elnur1502 <62846915+elnur1502@users.noreply.github.com>
2020-05-25 15:26:03 +0500

    Create meme.py

---
+++ meme.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_html(site):
+    r = requests.get(site)
+    return r.text
+
+
+def get_page_data(html):
+    soup = BeautifulSoup(html, 'lxml')
+    imgs = soup.find_all(
+
+    for img in imgs:
+        print img.get('src')
+
+
+def main():
+    url = 'https://9gag.com/meme'
+    get_page_data(get_html(url))
+
+
+if __name__ == '__main__':
+    main()

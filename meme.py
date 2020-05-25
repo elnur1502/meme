9d164e5f11db1684bbb731b5dde7abed9a90f457
elnur1502 <62846915+elnur1502@users.noreply.github.com>
2020-05-25 16:37:01 +0500

    Update meme.py

--- meme.py
+++ meme.py
@@ -9,7 +9,7 @@ def get_html(site):
 
 
 def get_page_data(html):
-    soup = BeautifulSoup(html, 'html.parser')
+    soup = BeautifulSoup(html, 'html.parser').text
     line = soup.find('img').text
     print line.get('src')
 

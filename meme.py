13f32a7d6609a1c09878da67142e06b74fdd7309
elnur1502 <62846915+elnur1502@users.noreply.github.com>
2020-05-25 18:38:50 +0500

    Update meme.py

--- meme.py
+++ meme.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 from urllib2 import urlopen
-html_doc = urlopen('http://1001mem.ru').read()
+html_doc = urlopen('https://9gag.com/meme').read()
 soup = BeautifulSoup(html_doc)
 
 for img in soup.find_all('img'):

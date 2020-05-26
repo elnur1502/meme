a6f2b33d9b0b0170c72a16c61791253c1087c122
elnur1502 <62846915+elnur1502@users.noreply.github.com>
2020-05-26 15:47:03 +0500

    Update meme.py

--- meme.py
+++ meme.py
@@ -1,46 +1,14 @@
-import requests
-from bs4 import BeautifulSoup
-import os
-
-
-def download_image(url, fileName):          #save image function
-    path = os.path.join("imgs", fileName)
-    f = open(path, 'wb')
-    f.write(requests.get(url).content)
-    f.close()
-
-
-def fetch_url(url):                        # fetching url
-    page = requests.get(url)
-    return page
-
-def parse_html(htmlPage):                  #parsing the url
-    soup = BeautifulSoup(htmlPage, "html.parser")
-    return soup
-
-
-def retrieve_jpg_urls(soup):
-
-    list_of_urls = soup.find_all('list')       #classes wanted
-    parsed_urls = []
-    for index in range(len(list_of_urls)):
-        try:
-            parsed_urls.append(soup.find_all('img')[index].attrs['src']) #img wanted inside class
-        except:
-            next
-    return parsed_urls
-
-
+import urllib2
+ 
 def main():
-    htmlPage = fetch_url("https://9gag.com/")
-    soup = parse_html(htmlPage.content)
-    jpgUrls = retrieve_jpg_urls(soup)
-    for index in range(len(jpgUrls)):
-        try:
-            download_image(jpgUrls[index], "savedpic{}.jpg".format(index))
-        except:
-            print("failed to parse image with url {}".format(jpgUrls[index]))
-    print("")
-
+   webUrl = urllib2.urlopen("https://9gag.com/meme")
+  
+#получаем код результата и выводим его
+   print "result code: " + str(webUrl.getcode()) 
+  
+# читаем данные с URL-адреса и выводим их   data
+ = webUrl.read()
+   print data
+ 
 if __name__ == "__main__":
-    main()
+  main()

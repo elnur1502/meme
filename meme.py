5ed8add90fd96ebfe63c5f06ec7ea840f4a64e24
elnur1502 <62846915+elnur1502@users.noreply.github.com>
2020-05-26 16:57:43 +0500

    Update meme.py

--- meme.py
+++ meme.py
@@ -2,12 +2,10 @@ import json
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://9gag.com/meme'
+url = 'https://9gag.com/gag/apGv05B'
 
 r = requests.get(url, headers={'User-Agent':'Mozilla/5.0'})
 s = BeautifulSoup(r.text, "lxml")
 img = s.find('img')
-img = json.load(img)
-for k,v in img.items():
-    if 'px' in k:
-        print(k)
+print(img)
+

85c72ab061b3f8895c01becec73949ceb045f644
elnur1502 <62846915+elnur1502@users.noreply.github.com>
2020-05-26 16:54:46 +0500

    Update meme.py

--- meme.py
+++ meme.py
@@ -2,12 +2,12 @@ import json
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://www.amazon.com/crocs-Unisex-Classic-Black-Women/dp/B0014C0LSY/ref=sr_1_2?_encoding=UTF8&qid=1560091629&s=fashion-womens-intl-ship&sr=1-2&th=1&psc=1'
+url = 'https://9gag.com/meme'
 
 r = requests.get(url, headers={'User-Agent':'Mozilla/5.0'})
 s = BeautifulSoup(r.text, "lxml")
-img = s.find(id="landingImage")['data-a-dynamic-image']
+img = s.find_all('img')
 img = json.loads(img)
 for k,v in img.items():
-    if '395' in k:
+    if 'px' in k:
         print(k)

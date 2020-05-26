16b5dd7f654c0d21390aef592d3ba8fcdb217c03
elnur1502 <62846915+elnur1502@users.noreply.github.com>
2020-05-26 12:21:55 +0500

    Update meme.py

--- meme.py
+++ meme.py
@@ -1,9 +1,9 @@
 from bs4 import BeautifulSoup
 from urllib2 import urlopen
-html_doc = urlopen('https://9gag.com/meme').read()
-soup = BeautifulSoup(html_doc)
+from fake_useragent import UserAgent
+UserAgent().chrome
+response = requests.get(page_link, headers={'User-Agent': UserAgent().chrome})
+response
 
-for img in soup.find_all('img'):
-    print img.get('src')
 
 

90a522dc3a5cb1b080b4ee9f0d9d8e1151a0fa22
elnur1502 <62846915+elnur1502@users.noreply.github.com>
2020-05-26 16:55:52 +0500

    Update meme.py

--- meme.py
+++ meme.py
@@ -6,7 +6,7 @@ url = 'https://9gag.com/meme'
 
 r = requests.get(url, headers={'User-Agent':'Mozilla/5.0'})
 s = BeautifulSoup(r.text, "lxml")
-img = s.find_all('img')
+img = s.find('img')
 img = json.load(img)
 for k,v in img.items():
     if 'px' in k:

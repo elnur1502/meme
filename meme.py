0e65154ca6c08a1a69c6cf07db53df68397a4951
elnur1502 <62846915+elnur1502@users.noreply.github.com>
2020-05-26 12:33:17 +0500

    Update meme.py

--- meme.py
+++ meme.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 from urllib2 import urlopen
 from fake_useragent import UserAgent
 UserAgent().chrome
-
+page_link = 'https://9gag.com/meme'
 
 def get_html(site):
   response = requests.get(page_link, headers={'User-Agent': UserAgent().chrome})

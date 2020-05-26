fb0753ec9ae4cdc0bd6358c3157fbbbe504613d8
elnur1502 <62846915+elnur1502@users.noreply.github.com>
2020-05-26 12:25:22 +0500

    Update meme.py

--- meme.py
+++ meme.py
@@ -9,5 +9,11 @@ response
 for key, value in response.request.headers.items():
   print(key+": "+value)
 
+html_doc = urlopen('https://9gag.com/meme').read()
+soup = BeautifulSoup(html_doc)
+
+for img in soup.find_all('img'):
+    print img.get('src')  
+
 
 
